8be0a62373dc2a01dde543af39e13f4eba33f345
Juan <juan_estrada_1@hotmail.com>
2025-10-21 15:29:07 -0600

    feat: add logic for badge

--- src/components/organisms/card.tsx
+++ src/components/organisms/card.tsx
@@ -2,8 +2,12 @@ import type { FC } from "react";
 import { CardContent, Card as CardShadcn } from "../ui/card";
 import type { CharacterProps } from "@/hooks/useFetchCharacters";
 import { Heart } from "lucide-react";
+import { statusColors } from "@/consts/styles";
 
 const Card: FC<CharacterProps> = ({ image, name, species, status }) => {
+  const dotColor =
+    statusColors[status.toLowerCase() as keyof typeof statusColors];
+
   return (
     <CardShadcn className="block relative py-0 rounded-[14px]">
       <div className="absolute flex items-center justify-center top-2 right-3 rounded-full bg-white w-9 h-9">
@@ -12,8 +16,8 @@ const Card: FC<CharacterProps> = ({ image, name, species, status }) => {
       <img src={image} className="object-cover rounded-t-[14px]" alt={name} />
       <CardContent className="py-4">
         <h2 className="font-bold text-18px">{name}</h2>
-        <div className="flex items-center justify-center w-[60px] h-[22px] bg-[#ECEEF2] rounded-[8px] mt-3 mb-2.5">
-          <div className="bg-[#00C950] rounded-full h-2 w-2 mr-2" />
+        <div className="inline-flex items-center justify-center h-[22px] bg-[#ECEEF2] rounded-[8px] mt-3 mb-2.5 px-3">
+          <div className={`${dotColor} rounded-full h-2 w-2 mr-2`} />
           <div className="text-[12px] font-medium">{status}</div>
         </div>
         <p className="text-[#6A7282] text-[14px]">{species}</p>
---
+++ src/consts/styles.ts
@@ -0,0 +1,5 @@
+export const statusColors = {
+  alive: "bg-[#00C950]",
+  dead: "bg-[#FB2C36]",
+  unknown: "bg-[#6A7282]",
+};
